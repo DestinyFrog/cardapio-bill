57b132fb88d9fc7e3d4dee382b19e5454c69caf4
DestinyFrog <pedrocalizto9@gmail.com>
2025-07-08 17:18:25 -0300

    a

--- src/App.svelte
+++ src/App.svelte
@@ -2,27 +2,38 @@
   import Cardapio from "./lib/Cardapio.svelte";
   import Carrinho from "./lib/Carrinho.svelte";
 
-    
+  let menu: "cardapio" | "carrinho" = "cardapio"
 </script>
 
 <header>
   <h1>BAR DO BILL</h1>
   
   <nav>
-    <button>Cardápio</button>
-    <button>Carrinho</button>
+    <button class="menu-button" on:click={ () => menu = "cardapio" }>Cardápio</button>
+    <button class="menu-button" on:click={ () => menu = "carrinho" }>Carrinho</button>
   </nav>
 </header>
 
 <main>  
-  <Cardapio></Cardapio>
-  <Carrinho></Carrinho>
+  {#if menu == "cardapio"}
+    <Cardapio></Cardapio>
+  {:else if menu == "carrinho"}
+    <Carrinho></Carrinho>
+  {/if}
 </main>
 
-<footer>
-  
-</footer>
-
 <style>
+  header {
+    position: sticky;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: auto;
+    background-color: gainsboro;
+  }
 
+  nav {
+    display: flex;
+    flex-direction: row;
+  }
 </style>
--- src/lib/Cardapio.ts
+++ src/lib/Cardapio.ts
@@ -1,65 +1,101 @@
-
 export interface prato {
-    nome: string,
-    valor: number,
-    tipo: string,
+  id: number;
+  nome: string;
+  valor: number;
+  tipo: string;
 }
 
-const cardapio: { pratos: prato[] } = {
-    "pratos": [
-        {
-            "nome": "Batata Frita com Cheddar e Bacon",
-            "valor": 35,
-            "tipo": "batata-frita"
-        },
-        {
-            "nome": "Batata Frita com Ketchup e Mostarda",
-            "valor": 35,
-            "tipo": "batata-frita"
-        },
-        {
-            "nome": "Espetinho de Carne",
-            "valor": 8,
-            "tipo": "espetinho"
-        },
-        {
-            "nome": "Espetinho de Queijo",
-            "valor": 8,
-            "tipo": "espetinho"
-        },
-        {
-            "nome": "Espetinho Misto (Carne + Linguiça)",
-            "valor": 8,
-            "tipo": "espetinho"
-        },
-        {
-            "nome": "Coca Cola",
-            "valor": 8,
-            "tipo": "bebida"
-        },
-        {
-            "nome": "Guaraná",
-            "valor": 8,
-            "tipo": "bebida"
-        },
-        {
-            "nome": "Cerveja",
-            "valor": 8,
-            "tipo": "bebida"
-        },
-        {
-            "nome": "Balde c\\ 10 cervejas",
-            "valor": 50,
-            "tipo": "bebida"
-        },
-        {
-            "nome": "Combo: Pinga + Energético",
-            "valor": 20,
-            "tipo": "bebida"
-        }
-    ]
-}
+const cardapio = {
+  pratos: [
+    {
+      id: 1,
+      nome: "Batata Frita com Cheddar e Bacon",
+      valor: 35,
+      tipo: "batata-frita",
+    },
+    {
+      id: 2,
+      nome: "Batata Frita com Ketchup e Mostarda",
+      valor: 35,
+      tipo: "batata-frita",
+    },
+    {
+      id: 3,
+      nome: "Espetinho de Carne",
+      valor: 8,
+      tipo: "espetinho",
+    },
+    {
+      id: 4,
+      nome: "Espetinho de Queijo",
+      valor: 8,
+      tipo: "espetinho",
+    },
+    {
+      id: 5,
+      nome: "Espetinho Misto (Carne + Linguiça)",
+      valor: 8,
+      tipo: "espetinho",
+    },
+    {
+      id: 6,
+      nome: "Coca Cola",
+      valor: 8,
+      tipo: "bebida",
+    },
+    {
+      id: 7,
+      nome: "Guaraná",
+      valor: 8,
+      tipo: "bebida",
+    },
+    {
+      id: 8,
+      nome: "Cerveja",
+      valor: 8,
+      tipo: "bebida",
+    },
+    {
+      id: 9,
+      nome: "Balde c\\ 10 cervejas",
+      valor: 50,
+      tipo: "bebida",
+    },
+    {
+      id: 10,
+      nome: "Combo: Pinga + Energético",
+      valor: 20,
+      tipo: "bebida",
+    },
+  ],
+  tipos: [
+    {
+      id: 1,
+      tipo: "batata-frita",
+      tituto: "Batata Frita",
+    },
+    {
+      id: 2,
+      tipo: "espetinho",
+      tituto: "Espetinhos",
+    },
+    {
+      id: 3,
+      tipo: "bebida",
+      tituto: "Bebidas",
+    },
+  ],
+};
 
 export async function getCardapio() {
-    return cardapio;
+  return cardapio.tipos.map((tipo) => {
+    const pratos = cardapio.pratos.filter(
+      ({ tipo: prato_tipo }) => tipo.tipo == prato_tipo
+    );
+
+    return {
+      ...tipo,
+      pratos,
+    };
+  });
 }
