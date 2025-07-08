bab59f287bd1428cb617642a3de7f6c2b0e38444
DestinyFrog <pedrocalizto9@gmail.com>
2025-07-08 16:29:43 -0300

    init

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import Cardapio from "./lib/Cardapio.svelte";
+  import Carrinho from "./lib/Carrinho.svelte";
+
+    
+</script>
+
+<header>
+  <h1>BAR DO BILL</h1>
+  
+  <nav>
+    <button>Cardápio</button>
+    <button>Carrinho</button>
+  </nav>
+</header>
+
+<main>  
+  <Cardapio></Cardapio>
+  <Carrinho></Carrinho>
+</main>
+
+<footer>
+  
+</footer>
+
+<style>
+
+</style>
---
+++ src/lib/Cardapio.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { getCardapio, type prato } from "./Cardapio";
+    import { formatarValor } from "./Functions";
+
+    let pratos: prato[]|null = $state(null);
+    onMount(() => getCardapio().then(data => pratos = data.pratos))
+</script>
+
+<div class="container">
+    {#if pratos}
+        {#each pratos as prato}
+            <p>{ prato.nome }</p>
+            <p>{ formatarValor(prato.valor) }</p>
+        {/each}
+    {/if}
+</div>
